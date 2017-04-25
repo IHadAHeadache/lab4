444cfeaf27ec16a928c7e8beed5d829959e9d594
CarUnderwater <orca2600@gmail.com>
2017-04-25 09:51:08 +0800

    lab4 done

---
+++ questionA.cpp
@@ -0,0 +1,13 @@
+double average(double * n1, double & n2)
+{
+return ((*n1 + n2) / 2.0);
+}
+int average(int n1, float n2)
+{
+return ((n1 + n2) / 2);
+}
+int main()
+{
+return 0;
+}
+
---
+++ questionB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+char a, *pa;
+int b, *pb;
+float c, *pc;
+double d, *pd;
+cout << sizeof(a) << " " << sizeof(pa) << endl;
+cout << sizeof(b) << " " << sizeof(pb) << endl;
+cout << sizeof(c) << " " << sizeof(pc) << endl;
+cout << sizeof(d) << " " << sizeof(pd) << endl;
+return 0;
+}
